0e67cb085a3b857235bc53857223ca5bfe8581e7
Yusuke Omori <omori@webware.co.jp>
2015-11-21 10:33:23 +0900

    Removal of PHP 5.3 deprecated set_magic_quotes_runtime in sfFileChache.class.php

--- lib/cache/sfFileCache.class.php
+++ lib/cache/sfFileCache.class.php
@@ -480,15 +480,19 @@ class sfFileCache extends sfCache
     {
       clearstatcache(true);
       $length = @filesize($path.$file);
-      $mqr = get_magic_quotes_runtime();
-      set_magic_quotes_runtime(0);
+      if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+        $mqr = get_magic_quotes_runtime();
+        set_magic_quotes_runtime(0);
+      }
       if ($this->readControl)
       {
         $hashControl = @fread($fp, 32);
         $length = $length - 32;
       }
       $data = ($length) ? @fread($fp, $length) : '';
-      set_magic_quotes_runtime($mqr);
+      if (version_compare(PHP_VERSION, '5.4.0', '<')) {
+        set_magic_quotes_runtime($mqr);
+      }
       if ($this->fileLocking)
       {
         @flock($fp, LOCK_UN);
